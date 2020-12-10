7593b2544e259283f4be99cf33067b21f300171a
Eric P <pcderic@gmail.com>
2020-12-10 08:22:59 -0500

    Support debugging

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HOST_SHARE="$PWD"
 CTR_SHARE="/app" 
-PUB="-p 127.0.0.1:5001:5001" 
+PUB="-p 127.0.0.1:5001:5001 -p 127.0.0.1:9229:9229" 
 OPTS="$*" 
 # Run container, remove it when completed, mount local dir to /app, name it with the directory name
 set -x
